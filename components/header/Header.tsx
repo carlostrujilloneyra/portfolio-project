c8e00039f0e8427871aeb95bc03d1fccaae374b3
Carlos Trujillo <augusto21_20@hotmail.com>
2023-09-04 20:00:47 -0500

    Primer commit

---
+++ components/header/Header.tsx
@@ -0,0 +1,11 @@
+
+export const Header = () => {
+	return (
+		<>
+			<header className="h-24 bg-[#000000] py-8 px-6">
+				
+			</header>
+		</>
+	)
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,17 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient': 'linear-gradient(90deg, hsla(240, 10%, 4%, 1) 30%, hsla(207, 10%, 23%, 1) 100%)'
       },
+      colors: {
+        darkGrey: '#1B1F24',
+        colorText: '#7D8590'
+      }
     },
   },
   plugins: [],
