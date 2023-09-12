3071cbd77e3bf5739cdbc9f44dbda3cdd42b135a
Carlos Trujillo <augusto21_20@hotmail.com>
2023-09-12 01:24:16 -0500

    Arreglando...

--- src/components/skills/SkillSection.tsx
+++ src/components/skills/SkillSection.tsx
@@ -8,7 +8,7 @@ export const SkillSection = () => {
     <>
       <section className="padding-x container-max-width py-6 lg:pt-9">
         <h2 className="subtitle-portfolio mb-4">Skills / Tecnologías</h2>
-        <div className="lg:grid-cols-[repeat(5,minmax(120px,max-content))]lg:gap-4 grid  grid-cols-[repeat(auto-fill,minmax(140px,max-content))] gap-3 py-5 min-[740px]:grid-cols-[repeat(4,minmax(150px,max-content))] lg:gap-4">
+        <div className="lg:grid-cols-[repeat(5,minmax(120px,max-content))]lg:gap-4 grid  grid-cols-[repeat(auto-fill,minmax(145px,max-content))] gap-3 py-5 min-[740px]:grid-cols-[repeat(4,minmax(150px,max-content))] lg:gap-4">
           <SkillList skills={skills} />
         </div>
       </section>
